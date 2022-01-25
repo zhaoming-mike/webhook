7af8cfa9eac593521e858c8e988c482cb837ac76
Mike <zhaoming23@gmail.com>
2022-01-25 21:29:40 +0800

    update

---
+++ dist/run.sh
@@ -0,0 +1,11 @@
+curl -X POST 'https://rancher.chinaeast2.cloudapp.chinacloudapi.cn/hooks?pipelineId=p-ctzzr:p-tb29w' \
+-H 'Content-type: application/json' \
+-H 'User-Agent: GitHub-Hookshot/9574af4' \
+-H 'X-GitHub-Delivery: 389251c2-7dd8-11ec-8fe1-f6fa348b9c6f' \
+-H 'X-GitHub-Event: push' \
+-H 'X-GitHub-Hook-ID: 339905122' \
+-H 'X-GitHub-Hook-Installation-Target-ID: 451852659' \
+-H 'X-GitHub-Hook-Installation-Target-Type: repository' \
+-H 'X-Hub-Signature: sha1=fe98150181e6fa8664387770a0f8f12fb78cdee2' \ 
+-H 'X-Hub-Signature-256: sha256=3d25c824f8bc89ad6b8612638f35fc9bb0c55a3eb49acc3c0dca85cb277143b5' \
+-d @1.json
